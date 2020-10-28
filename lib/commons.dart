2f0c157b4824806fa9497d81a3bfd46c97f6cfa1
Durrell Gemuh <durrellgemuh07@gmail.com>
2020-10-28 06:54:26 +0100

    code refactor

---
+++ lib/commons.dart
@@ -0,0 +1,5 @@
+
+import 'package:flutter/material.dart';
+
+Color primaryColor = Colors.teal.shade900;
+Color bgColor = Colors.teal.shade800;
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mi_card/commons.dart';
 
 void main() {
   runApp(MyApp());
@@ -7,7 +8,17 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MyHomePage();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: bgColor,
+        appBar: AppBar(
+          title: Text('Mi Card'),
+          backgroundColor: primaryColor,
+        ),
+        body: MyHomePage(),
+      ),
+    );
   }
 }
 
@@ -19,32 +30,59 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Business Card'),
-      ),
-      body: Container(
-        child: Column(
-          children: [
-            CircleAvatar(
-              child: Image.asset('image link'),
-            ),
-            Text('Da Suspect'),
-            Card(
-              child: ListTile(
-                leading: Icon(Icons.phone),
-                title: Text('xxx-xxx-xxx'),
-              ),
-            ),
+    String name = 'Durrell Gemuh A.';
+    String image = 'assets/images/pp.jpg';
+    String number = '+237 671 305 865';
+    String email = 'durrellgemuh07@gmail.com';
 
-            Card(
-              child: ListTile(
-                leading: Icon(Icons.email),
-                title: Text('dasuspect@example.com'),
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          CircleAvatar(
+            radius: 70,
+            child: Image.asset('$image'),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Text(
+            '$name',
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(
+            height: 50,
+            child: Divider(
+              color: Colors.white70,
+            ),
+          ),
+          Card(
+            child: ListTile(
+              leading: Icon(
+                Icons.phone,
+                color: primaryColor,
               ),
-            )
-          ],
-        ),
+              title: Text('$number'),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              leading: Icon(
+                Icons.email,
+                color: primaryColor,
+              ),
+              title: Text('$email'),
+            ),
+          ),
+          Container(
+            child: Text(
+              "Need one, Contact me",
+              style: TextStyle(color: Colors.blueGrey.shade900),
+            ),
+            margin: EdgeInsets.all(30),
+          )
+        ],
       ),
     );
   }
